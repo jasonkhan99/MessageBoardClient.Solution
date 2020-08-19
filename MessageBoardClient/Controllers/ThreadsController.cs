57fe6f4b8219f04b7cb8596335db372e11787483
Ian Scott <chesnekov@gmail.com>
2020-08-19 16:05:19 -0700

    adds notes to ThreadsController.cs, adds UsersController.cs empty file.
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- MessageBoardClient/Controllers/ThreadsController.cs
+++ MessageBoardClient/Controllers/ThreadsController.cs
@@ -1,9 +1,14 @@
 
 
+    // Index  -- nothing passed
+    // Details  -- pass id
+    // Delete View -- pass id
+    // Delete [HttpPost] -- pass id
+    // Create View -- Pass list of Boards
+    // Create [post] -- pass object
+    // Edit View -- pass id
+    // Edit [Post] - pass object
 
-    // Add Thread -- View Only, pass in Parent Board
-    // Add Thread [Post] -- Creates a thread with Parent Board
 
-    // ------ Move These to Thread Controller? -------
     // Thread Edit View -- Pass in Specific Thread
     // Thread Edit [Post] -- Save Thread edit changes
