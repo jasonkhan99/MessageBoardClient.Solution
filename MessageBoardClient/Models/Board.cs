6be0c58247e9be925ff41accf295ff71f09f25a1
Ian Scott <chesnekov@gmail.com>
2020-08-19 11:35:49 -0700

    add  static methods to match up with back-end api to Board model for all back-end api functinoality relating to boards
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- MessageBoardClient/Models/Board.cs
+++ MessageBoardClient/Models/Board.cs
@@ -16,6 +16,17 @@ namespace MessageBoardClient.Models
       this.Threads = new HashSet<Thread>();
     }
 
+    public static List<Board> GetBoards()
+    {
+      var apiCallTask = apiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Board> boardList = JsonConvert.DeserializeObject<List<Board>>(jsonResponse.ToString());
+
+      return boardList;
+    }
+
     public static Board GetDetails(int id)
     {
       var apiCallTask = ApiHelper.Get(id);
@@ -23,8 +34,48 @@ namespace MessageBoardClient.Models
 
       JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
       Board board = JsonConvert.DeserializeObject<Board>(jsonResponse.ToString());
-
       return board;
+    }
+
+    public static void Post(Board board)
+    {
+      string jsonBoard = JsonConvert.SerializeObject(board);
+      var apiCallTask = ApiHelper.Post(jsonBoard);
+    }
+
+    public static void Put(Board board)
+    {
+      string jsonBoard = jsonConvert.SerializeObject(board);
+      var apiCallTask = ApiHelper.Put(board.BoardId, jsonboard);
+    }
+
+    public static void Delete(int id)
+    {
+      var apiCallTask = ApiHelper.Delete(id);
+    }
+
+    public static List<Thread> GetThreads(int boardId)
+    {
+      // UPdate this to reference Getting all threads!
+      var apiCallTask = apiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Threads> threadList = JsonConvert.DeserializeObject<List<Thread>>(jsonResponse.ToString());
+
+      return threadList;
+    }
+    
+    public static void PutThread(Thread thread)
+    {
+      string jsonThread = jsonConvert.SerializeObject(thread);
+      var apiCallTask = ApiHelper.Put(thread.ThreadId, jsonthread);
+    }
+    
+    public static void PostThread(Thread thread)
+    {
+      string jsonThread = jsonConvert.SerializeObject(thread);
+      var apiCallTask = ApiHelper.Post(thread.ThreadId, jsonthread);
     }
   }
 }
