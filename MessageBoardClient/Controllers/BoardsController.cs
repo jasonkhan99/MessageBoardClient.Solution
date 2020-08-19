d7bb467bd883c8ebd0ae9c2b98256d54e3055782
Ian Scott <chesnekov@gmail.com>
2020-08-19 15:52:51 -0700

    update BoardsController with all required routes for CRUD and RESTful funcitonality
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

---
+++ MessageBoardClient/Controllers/BoardsController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MessageBoardClient.Models;
+
+namespace MessageBoardClient.Controllers
+{
+  public class BoardsController : Controller
+  {
+    
+    public IActionResult Index()
+    {
+      var allBoards = Board.GetBoards();
+      return View(allBoards);
+    }
+
+    public IActionResult Details(int id)
+    {
+      var boardDetails = Board.GetDetails(id);
+      return View(boardDetails);
+    }
+    
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Board board)
+    {
+      Board.Post(board);
+      return RedirectToAction("Index");
+    }
+
+    public IActionResult Edit(int id)
+    {
+      var boardChange = Board.GetDetails(id);
+      return View(boardChange);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Board board)
+    {
+      Board.Put(board);
+      return RedirectToAction("Details", board.BoardId);
+    }
+
+    public IActionResult Delete (int boardId)
+    {
+      var boardDetails = Board.GetDetails(boardId);
+      return View(boardDetails);
+    }
+
+    [HttpPost]
+    public IActionResult Delete (int boardId)
+    {
+      Board.Delete(boardId);
+      return RedirectToAction("Index");
+    }
+  }
+}
---
+++ MessageBoardClient/Controllers/ThreadsController.cs
@@ -0,0 +1,9 @@
+
+
+
+    // Add Thread -- View Only, pass in Parent Board
+    // Add Thread [Post] -- Creates a thread with Parent Board
+
+    // ------ Move These to Thread Controller? -------
+    // Thread Edit View -- Pass in Specific Thread
+    // Thread Edit [Post] -- Save Thread edit changes
